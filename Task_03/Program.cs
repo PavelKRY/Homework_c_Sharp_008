f49a9a934b25b65b1fae5fcbe0591565246d9345
PavelKRY <kryukov.p14@gmail.com>
2022-09-16 11:41:10 +0500

    Task_03 issue_new good

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -7,38 +7,59 @@
 // 10 11 55
 // 33 41 23
 // 17 28 34
-
-int [,] array = new int [5,5];
+Console.WriteLine("Введите число строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, column];
+if(array.GetLength(0)*array.GetLength(1)<=50)
+{
 Random rnd = new Random();
 void FillArray(int[,] array)
 {
-    int tmp = 10;
-    for(int i = 0; i< array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j< array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            
-            array[i,j] = tmp;
-            tmp+=1;
+
+            array[i, j] = rnd.Next(10, 100);
+            for (int m = 0; m < array.GetLength(0); m++)
+            {
+                for (int n = 0; n < array.GetLength(1); n++)
+                {
+
+                    if (array[i, j] == array[m, n])
+                    {
+                        array[m, n] = rnd.Next(10, 100);
+                    }
+                }
+            }
         }
     }
 }
+
+
+
 void PrintArray(int[,] array)
 {
-    for(int i = 0; i< array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j< array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(" " + array[i,j]);
+            Console.Write(" " + array[i, j]);
         }
-        Console.WriteLine(" - Строка номер " + (i+1));
+        Console.WriteLine(" - Строка номер " + (i + 1));
     }
 }
 
 FillArray(array);
 PrintArray(array);
 Console.WriteLine(string.Empty);
-
+}
+else
+{
+    Console.WriteLine("Введите массив, менее 50 значений");
+}
 
 
 
